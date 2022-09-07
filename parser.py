6bdc21178683c53613b65ac9d6d65b994d1e5b54
miron4ik <miron4ik.life@yandex.ru>
2022-09-07 16:11:18 +0300

    Parse work.ua

--- parser.py
+++ parser.py
@@ -1,19 +1,33 @@
+import time
+import requests
 from bs4 import BeautifulSoup
   
 def parse(html):
   soup = BeautifulSoup(data, 'lxml')
-  reviews = soup.findAll('div', { 'itemprop': 'review' })
-
-  for review in reviews:
-    author = review.find('meta', { 'itemprop': 'author' })['content']
-    description = review.find('meta', { 'itemprop': 'description' })['content']
-    rating = review.find('meta', { 'itemprop': 'ratingValue' })['content']
-    datePublished = review.find('meta', { 'itemprop': 'datePublished' })['content']
+  vacancies = soup.findAll('div', { 'class': 'job-link' })
+  
+  for vacancy in vacancies:
+    href = vacancy.find('a')['href']
+    title = vacancy.h2.text.strip()
+    description = vacancy.p.text.strip()
+    info = vacancy.b.text.strip()
     
-    print(author, description, rating, datePublished)
+    print('Ссылка', href, sep=': ')
+    print('Название', title, sep=': ')
+    print('Описание', description, sep=': ')
+    print('Информация', info, sep=': ')
+    print()
 
-file = open('market.html', 'r', encoding='utf-8')
-data = file.read()
-file.close()
+start_page = 1
+max_page = 5
 
-parse(data)
+for page in range(start_page, max_page + 1):
+  url = 'https://www.work.ua/ru/jobs-kyiv/?page=' + str(page)
+  
+  response = requests.get(url)
+  response.encoding = 'utf-8'
+
+  data = response.text
+  parse(data)
+  
+  time.sleep(10)
